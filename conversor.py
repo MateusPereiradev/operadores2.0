4e5022014df7bac5a6f18030ba8cc391eecc1a52
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-10 20:52:42 -0300

    conversor de real para dólar

--- conversor.py
+++ conversor.py
@@ -1,4 +1,4 @@
-carteira= float(input("Digite aqui o valor que você tem na carteira:"))
-print(f"Referente ao valor de R${carteira:.2f}, o valor em dolar é US${carteira/5.40:.2f}")
+carteira= float(input('Digite aqui o valor que você tem em sua carteira: R$'))
+print(f'Referente ao valor que contém em sua carteira de R${carteira:.2f} o valor convertido em dólar é US${carteira/5.40:.2f}!')
 
 # crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
