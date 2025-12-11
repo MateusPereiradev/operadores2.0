049e3f8a747f63db30948e99d9651b81ea3d1b85
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-11 20:27:58 -0300

    Média aritmética

--- mediaaritmetica.py
+++ mediaaritmetica.py
@@ -1,6 +1,6 @@
-n1= float(input("Digite aqui a primeira nota do aluno:"))
-n2= float(input("Digite aqui a segunda nota do aluno:"))
-total= n1+n2
-print(f"A média do aluno referente a primeira nota {n1} e a segunda {n2} é {total/2:.2f}")
+nota_1= float(input('Digite aqui a primeira nota do aluno:'))
+nota_2= float(input('Digite aqui a segunda nota do aluno:'))
+print(f'Referente a primeira nota do aluno de {nota_1} e a segunda de {nota_2} a média do aluno é {(nota_1+nota_2)/2}!')
+
 
 # desenvolva o programa que leia as duas notas de um aluno, calcule e mostre sua média.
