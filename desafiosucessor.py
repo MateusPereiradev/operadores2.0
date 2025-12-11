cfec641b36658c233ddc3de34822b8db281e3e7d
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-11 20:24:12 -0300

    sucessor e antecessor

--- desafiosucessor.py
+++ desafiosucessor.py
@@ -1,8 +1,6 @@
-n = int(input("digite aqui um número:"))
-print(f"O sucessor de {n} é equivalente à {n + 1}")
-print(f"O antecessor de {n} é equivalente à {n - 1}")
-print(f"O dobro de {n} é equivalente à {n * 2}")
-print(f"O triplo de {n} é equivalente à {n * 3}")
-print(f"O valor de {n} divido por 3 é igual à {n / 3:.2f}")
+num= int(input('Digite aqui um número inteiro:'))
+print(f'O Sucessor de {num} é {num+1}!')
+print(f'O antecessor de {num} é {num-1}!')
+print('Fim do programa de sucessor e antecessor!')
 
 # faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
