fc011f733e553c9b6b8e4f56aebafa2a318a7025
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-16 20:56:13 -0300

    Reajuste salarial

--- Reajuste.py
+++ Reajuste.py
@@ -1,7 +1,6 @@
-salário= float(input('Digite aqui o valor do seu salário:'))
-novo_salário= salário+salário*0.15
-print(f'Referente ao salário do funcionário de R${salário:.2f} o salário com aumento de 15% será R${novo_salário:.2f}!')
-
+salário= float(input('Digite aqui o seu salário:R$'))
+novo_salario=salário+salário*0.15
+print(f'Referente ao salário de R${salário:.2f}, o novo salário com 15% de aumento será R${novo_salario:.2f}!')
 
 
 # faça um algoritmo que leia um salário de um funcionário e mostre seu novo salário, com 15% de aumento.
