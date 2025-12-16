368943d64348999dd5cbd5065c0bc76511d5203c
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-16 20:54:19 -0300

    Produto com desconto e a prazo

--- prazoavista.py
+++ prazoavista.py
@@ -1,8 +1,9 @@
-valor_produto= float(input('Digite aqui o valor do produto que deseja comprar:'))
-desconto= valor_produto-valor_produto*0.05
-prazo= valor_produto+valor_produto*0.08
-print(f'O valor do produto com desconto de 5% é R${desconto:.2f}!\nO valor do produto a prazo com 8% de juros é R${prazo:.2f}!')
-
+produto= float(input('Digite aqui o preço do produto que deseja comprar:R$'))
+desconto= produto-produto*0.05
+prazo= produto+produto*0.08
+print(f'O valor do produto com 5% de desconto é R${desconto:.2f}!')
+print(f'O valor do produto a prazo com 8% de acréscimo é R${prazo:.2f}!')
+print('Fim do programa!')
 
 
 # mostre o valor de um produto com 5% de desconto e outro valor a prazo com 8% de acréscimo em cima do valor do produto.
