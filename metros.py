aa934fef048ae73cea7813d661b559c8b392a06a
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-16 20:49:02 -0300

    Convertendo metros

--- metros.py
+++ metros.py
@@ -1,6 +1,6 @@
-metros= float(input('Digite aqui o valor em metros:'))
-print(f'Referente ao valor em metros, o valor em centimetros é {metros*100}CM!')
-print(f'Referente ao valor em metros, o valor em milímetros é {metros*1000}MM!')
-print(f'Referente ao valor em metros, o valor em km é {metros/1000}!')
+metros= float(input('Digite aqui um valor em metros:'))
+print(f'O valor de {metros}m convertido em centímetros é {metros*100}cm!')
+print(f'O valor de {metros}m convertido em milímetros é {metros*1000}mm!')
+print(f'O valor de {metros}m convertido em km é {metros/1000}km!')
 
 # escreva um programa, que leia um valor em metros e o exiba convertido em centímetros e mílimetros.
