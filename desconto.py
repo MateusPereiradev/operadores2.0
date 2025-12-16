f0eb863fea0573be1b18c2616cfb27f11a071469
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-16 20:41:35 -0300

    Desconto do produto

--- desconto.py
+++ desconto.py
@@ -1,4 +1,5 @@
-valor_produto= float(input('Digite aqui o valor do produto que deseja comprar: R$'))
-print(f'Referente ao valor do produto R${valor_produto:.2f} o valor com 5% de desconto é R${valor_produto-valor_produto*0.05:.2f}!')
+produto= float(input('Digite aqui o preço do produto que deseja comprar:R$'))
+print(f'Referente ao preço do produto de R${produto:.2f} com 5% de desconto o mesmo será {produto-produto*0.05:.2f}!')
+
 
 # faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
