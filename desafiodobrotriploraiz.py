281559e490087d12eb4265736a30d90db6cfb768
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-12-16 20:35:56 -0300

    Dobro, triplo e raiz de um número

--- desafiodobrotriploraiz.py
+++ desafiodobrotriploraiz.py
@@ -2,8 +2,7 @@ from math import sqrt
 num= float(input('Digite aqui um número:'))
 print(f'O dobro de {num} é {num*2}!')
 print(f'O triplo de {num} é {num*3}!')
-print(f'A raiz quadrada de {num} é {sqrt(num):.2f}!')
-print('Fim do programa de dobro, triplo e raiz quadrada!')
-
+print(f'A raiz quadrada de {num} é {sqrt(num):.2f}')
+print('Fim do programa!')
 
 # indique um número e informe seu dobro, triplo e raiz quadrada.
